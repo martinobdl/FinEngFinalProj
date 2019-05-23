a84cd96a505ccd82418eba15b23a9c0eb77d3fe5
akira2357 <martino.n.bernasconi@gmail.com>
2019-05-24 00:14:26 +0200

    first code commit

---
+++ matlab/main.m
@@ -0,0 +1,16 @@
+%% run with main.m
+
+clear all
+close all
+
+%% read Data
+
+data = readData();
+
+%% Protfolios definitions
+
+N_obligors = 50;
+confLevel1 = 0.99;
+confLevel2 = 0.999;
+
+%% 
---
+++ matlab/readData.m
@@ -0,0 +1,21 @@
+function data = readData()
+% data = readData()
+% Read the data file into a struct with
+% year, specultaive grade default rate, total defalut rate
+% and recovery rate.
+%
+% @inputs: None
+%
+% @outputs: data: struct with year, DG_SG, DG_All, RR
+%
+
+    fileName = '../data/Data.csv';
+    matrixData = dlmread(fileName,',',1,0);
+    
+    data.year   = matrixData(:,1);
+    data.DG_SG  = matrixData(:,2);
+    data.DG_All = matrixData(:,3);
+    data.RR     = matrixData(:,4);
+    
+end
+
